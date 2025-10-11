f0d8b62321831bf5ff10ae8839c943d2783018cf
Kilikaevrv <coffeine5555@gmail.com>
2025-10-11 15:10:18 +0300

    Task2

--- Tyuiu.KilikaevRV.Sprint2.Task2.V2.Lib/DataService.cs
+++ Tyuiu.KilikaevRV.Sprint2.Task2.V2.Lib/DataService.cs
@@ -5,51 +5,26 @@ namespace Tyuiu.KilikaevRV.Sprint2.Task2.V2.Lib
 {
 
     
-        public class PointChecker : ISprint2Task2V2
+        public class DataService : ISprint2Task2V2
         {
-            /// <summary>
-            /// Проверяет, находится ли точка внутри круга заданного радиуса
-            /// </summary>
-            public static bool IsIncircle(int x, int y, int radius)  // Изменил на IsIncircle
+            public bool CheckDotInShadedArea(int x, int y)
             {
-                return (x * x + y * y) <= (radius * radius);
+                // Определяем заштрихованную область
+                // Предположим, что это прямоугольник + круг + треугольник
+
+                // Прямоугольник: от (3,3) до (10,8)
+                bool inRectangle = (x >= 3 && x <= 10 && y >= 3 && y <= 8);
+
+                // Круг с центром в (7,7) и радиусом 3
+                bool inCircle = (Math.Pow(x - 7, 2) + Math.Pow(y - 7, 2) <= 9);
+
+                // Треугольник с вершинами (5,10), (9,10), (7,13)
+                bool inTriangle = (y >= 10 && y <= 13 &&
+                                  x >= 5 + (y - 10) &&
+                                  x <= 9 - (y - 10));
+
+                // Точка находится в заштрихованной области, если попадает в любую из фигур
+                return inRectangle || inCircle || inTriangle;
             }
-
-            /// <summary>
-            /// Проверяет, находится ли точка внутри прямоугольника
-            /// </summary>
-            public static bool IsInRectangle(int x, int y, int x1, int y1, int x2, int y2)
-            {
-                return x >= x1 && x <= x2 && y >= y1 && y <= y2;
-            }
-
-            /// <summary>
-            /// Проверяет, находится ли точка в кольце между двумя радиусами
-            /// </summary>
-            public static bool IsInRing(int x, int y, int innerRadius, int outerRadius)
-            {
-                double distanceSquared = x * x + y * y;
-                int innerSquared = innerRadius * innerRadius;
-                int outerSquared = outerRadius * outerRadius;
-                return distanceSquared >= innerSquared && distanceSquared <= outerSquared;
-            }
-
-            /// <summary>
-            /// Проверяет нахождение точки в комбинированной заштрихованной области
-            /// </summary>
-            public static bool IsInShadedArea(int x, int y)
-            {
-                // Для точки (9,6) используем достаточно большой радиус
-                bool inCircle = IsIncircle(x, y, 11);  // Теперь вызываем IsIncircle
-                bool inSquare = IsInRectangle(x, y, -3, -3, 3, 3);
-                bool inRing = IsInRing(x, y, 2, 4);
-
-                return inCircle || inSquare || inRing;
-            }
-
-        public bool CheckDotInShadedArea(int x, int y)
-        {
-            throw new NotImplementedException();
         }
-    }
 }
--- Tyuiu.KilikaevRV.Sprint2.Task2.V2.Test/DataServiceTest.cs
+++ Tyuiu.KilikaevRV.Sprint2.Task2.V2.Test/DataServiceTest.cs
@@ -2,117 +2,29 @@
 
 
 
-
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tyuiu.KilikaevRV.Sprint2.Task2.V2.Lib;
 
-namespace Tyulu.KilikaevRY.Sprint2.Task2.V2
+namespace Tyuiu.KilikaevRV.Sprint2.Task2.V2.Test
 {
     [TestClass]
-    public class PointCheckerTests
+    public class DataServiceTest
     {
         [TestMethod]
-        public void IsInCircle_PointInsideCircle_ReturnsTrue()
+        public void ValidCheckDotInShadedArea()
         {
-            // Arrange
-            int x = 3, y = 4, radius = 5;
+            DataService ds = new DataService();
 
-            // Act
-            bool result = PointChecker.IsIncircle(x, y, radius);
+            // Точки внутри заштрихованной области
+            Assert.AreEqual(true, ds.CheckDotInShadedArea(5, 5));  // в прямоугольнике
+            Assert.AreEqual(true, ds.CheckDotInShadedArea(7, 7));  // в центре круга
+            Assert.AreEqual(true, ds.CheckDotInShadedArea(7, 11)); // в треугольнике
+            Assert.AreEqual(true, ds.CheckDotInShadedArea(8, 6));  // на границе круга
 
-            // Assert
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void IsInCircle_PointOutsideCircle_ReturnsFalse()
-        {
-            // Arrange
-            int x = 4, y = 4, radius = 5;
-
-            // Act
-            bool result = PointChecker.IsIncircle(x, y, radius);
-
-            // Assert
-            Assert.IsFalse(result);
-        }
-
-        [TestMethod]
-        public void IsInRectangle_PointInsideRectangle_ReturnsTrue()
-        {
-            // Arrange
-            int x = 2, y = 2;
-
-            // Act
-            bool result = PointChecker.IsInRectangle(x, y, -3, -3, 3, 3);
-
-            // Assert
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void IsInRectangle_PointOutsideRectangle_ReturnsFalse()
-        {
-            // Arrange
-            int x = 4, y = 4;
-
-            // Act
-            bool result = PointChecker.IsInRectangle(x, y, -3, -3, 3, 3);
-
-            // Assert
-            Assert.IsFalse(result);
-        }
-
-        [TestMethod]
-        public void IsInRing_PointInRing_ReturnsTrue()
-        {
-            // Arrange
-            int x = 3, y = 0;
-
-            // Act
-            bool result = PointChecker.IsInRing(x, y, 2, 4);
-
-            // Assert
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void IsInRing_PointOutsideRing_ReturnsFalse()
-        {
-            // Arrange
-            int x = 5, y = 0;
-
-            // Act
-            bool result = PointChecker.IsInRing(x, y, 2, 4);
-
-            // Assert
-            Assert.IsFalse(result);
-        }
-
-        [TestMethod]
-        public void IsInShadedArea_PointInShadedArea_ReturnsTrue()
-        {
-            // Arrange
-            int x = 0, y = 0;  // Точка в начале координат должна быть в заштрихованной области
-
-            // Act
-            bool result = PointChecker.IsInShadedArea(x, y);
-
-            // Assert
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void IsInShadedArea_PointNotInShadedArea_ReturnsFalse()
-        {
-            // Arrange
-            int x = 10, y = 10;  // Точка далеко от всех областей
-
-            // Act
-            bool result = PointChecker.IsInShadedArea(x, y);
-
-            // Assert
-            Assert.IsFalse(result);
+            // Точки вне заштрихованной области
+            Assert.AreEqual(false, ds.CheckDotInShadedArea(1, 1));  // далеко
+            Assert.AreEqual(false, ds.CheckDotInShadedArea(15, 15)); // далеко
+            Assert.AreEqual(false, ds.CheckDotInShadedArea(5, 2));   // под прямоугольником
+            Assert.AreEqual(false, ds.CheckDotInShadedArea(10, 10)); // над прямоугольником, но не в треугольнике
         }
     }
 }
