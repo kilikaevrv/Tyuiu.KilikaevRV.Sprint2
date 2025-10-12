bc7da01094b333b71c86f166e23a713fbc9aa39a
Kilikaevrv <coffeine5555@gmail.com>
2025-10-12 13:50:21 +0300

    Task6

--- Tyuiu.KilikaevRV.Sprint2.Task6.V6.Test/DataServiceTest.cs
+++ Tyuiu.KilikaevRV.Sprint2.Task6.V6.Test/DataServiceTest.cs
@@ -12,22 +12,22 @@
                 DataService ds = new DataService();
 
                 // Тест: Дама пик (m=1, k=12)
-                Assert.AreEqual("Дама пик", ds.FindCardNameAndSuit(1, 12));
+                Assert.AreEqual("дама пик", ds.FindCardNameAndSuit(1, 12));
 
                 // Тест: Шестерка бубен (m=3, k=6)
-                Assert.AreEqual("Шестерка бубен", ds.FindCardNameAndSuit(3, 6));
+                Assert.AreEqual("шестерка бубен", ds.FindCardNameAndSuit(3, 6));
 
                 // Тест: Туз червей (m=4, k=14)
-                Assert.AreEqual("Туз червей", ds.FindCardNameAndSuit(4, 14));
+                Assert.AreEqual("туз червей", ds.FindCardNameAndSuit(4, 14));
 
                 // Тест: Валет треф (m=2, k=11)
-                Assert.AreEqual("Валет треф", ds.FindCardNameAndSuit(2, 11));
+                Assert.AreEqual("валет треф", ds.FindCardNameAndSuit(2, 11));
 
                 // Тест: Король пик (m=1, k=13)
-                Assert.AreEqual("Король пик", ds.FindCardNameAndSuit(1, 13));
+                Assert.AreEqual("король пик", ds.FindCardNameAndSuit(1, 13));
 
                 // Тест: Десятка червей (m=4, k=10)
-                Assert.AreEqual("Десятка червей", ds.FindCardNameAndSuit(4, 10));
+                Assert.AreEqual("десятка червей", ds.FindCardNameAndSuit(4, 10));
             }
 
             [TestMethod]
@@ -36,13 +36,13 @@
                 DataService ds = new DataService();
 
                 // Граничные значения
-                Assert.AreEqual("Шестерка пик", ds.FindCardNameAndSuit(1, 6));
-                Assert.AreEqual("Туз червей", ds.FindCardNameAndSuit(4, 14));
+                Assert.AreEqual("шестерка пик", ds.FindCardNameAndSuit(1, 6));
+                Assert.AreEqual("туз червей", ds.FindCardNameAndSuit(4, 14));
 
                 // Неверные значения
-                Assert.AreEqual("Неизвестная карта неизвестной масти", ds.FindCardNameAndSuit(5, 12));
-                Assert.AreEqual("Неизвестная карта неизвестной масти", ds.FindCardNameAndSuit(1, 15));
-                Assert.AreEqual("Неизвестная карта неизвестной масти", ds.FindCardNameAndSuit(0, 10));
+                Assert.AreEqual("неизвестная карта неизвестной масти", ds.FindCardNameAndSuit(5, 12));
+                Assert.AreEqual("неизвестная карта неизвестной масти", ds.FindCardNameAndSuit(1, 15));
+                Assert.AreEqual("неизвестная карта неизвестной масти", ds.FindCardNameAndSuit(0, 10));
             }
         }
     }
